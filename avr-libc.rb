42ebbbca4b448bc086568f8b12c7c82633aa4bcc
Robert Quattlebaum <darco@deepdarc.com>
2013-06-28 09:56:31 -0700

    avr-libc: Added support for building trunk with '--HEAD'.

--- avr-libc.rb
+++ avr-libc.rb
@@ -4,8 +4,17 @@ class AvrLibc < Formula
   url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2'
   homepage 'http://www.nongnu.org/avr-libc/'
   sha256 '67a36bdb43a16f9b0512ba09584d4785f9bce6c057393f1020a5552e096e34ce'
+  head 'svn://svn.sv.gnu.org/avr-libc/trunk/avr-libc/'
 
   depends_on 'avr-gcc'
+
+  if build.head?
+    # These are required on the HEAD build because the configure
+    # script doesn't live on the subversion repository.
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
 
   def install
     # brew's build environment is in our way
@@ -14,6 +23,8 @@ class AvrLibc < Formula
     ENV.delete 'LD'
     ENV.delete 'CC'
     ENV.delete 'CXX'
+
+    system "./bootstrap" if build.head?
 
     avr_gcc = Formula.factory('avr-gcc')
     build = `./config.guess`.chomp
